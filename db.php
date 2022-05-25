6f9f954a80e277d78b447010d1cb53cfcbaa6d7a
nehabht <ne.bhatti2@icloud.com>
2022-05-26 01:17:05 +0200

    add class and cards

---
+++ db.php
@@ -0,0 +1,46 @@
+<?php
+
+class Movie {
+
+    public $title;
+    //pubblic $description;
+    public $year;
+    public $rating;
+    public $image;
+
+
+    function __construct($title, $rating, $year, $image){
+
+        $this->title = $title;
+        $this->year = $year;
+        $this->image = $image;
+        $this->rating = $rating;
+
+    }
+
+
+
+
+
+
+
+};
+
+
+$movies = [
+
+    new Movie ('La Fantastica Signora Maisel - Stagione 4', '8.7', '2022', 'https://n7m3z4b2.stackpathcdn.com/wp-content/uploads/2022/02/La-fantastica-signora-Maisel-4-Poster.webp'),
+    new Movie ('La leggenda di Vox Machina - Stagione 1', '8.4', '2022', 'https://sm.ign.com/ign_it/tv/t/the-legend/the-legend-of-vox-machina_w1bt.jpg'),
+    new Movie ('The Expanse - Stagione 6', '8.5', '2022', 'https://tv-fanatic-res.cloudinary.com/iu/s--xIVP1Vbv--/c_scale,f_auto,h_1078,q_auto,w_696/v1564260220/the-expanse-season-4-poster'),
+
+
+
+
+];
+
+
+//  è definita una classe ‘Movie’
+// => all'interno della classe sono dichiarate delle variabili d'istanza
+// => all'interno della classe è definito un costruttore
+// => all'interno della classe è definito almeno un metodo
+//  vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+include __DIR__ . "/db.php";
+
+// echo '<pre>';
+// var_dump($movies);
+// echo '</pre>';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+    
+</head>
+<body>
+
+    <header style="height: 100px; background-color: Black; margin-bottom:30px;">
+        <h1 class="text-center pt-4" style="color:white;">Watching List</h1>
+
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row row-cols-3">
+                <?php foreach ($movies as $movie) : ?>
+                    <div class="col h-100">
+                        <div class="card ">
+                            <div class="card-top d-flex justify-content-center" >
+                                <img class="img-fluid" style="width: auto; height: 500px;" src="<?= $movie->image ?>" alt="">
+                            </div>
+                            <div class="card-bottom ms-2 mt-2">
+                                <h5><?= $movie->title ?></h5>
+                                <p class="d-flex flex-column"> Rating:
+                                    <?= $movie->rating ?>
+                                    <span> Year:
+                                    <?= $movie->year ?>
+                                    </span>
+                                </p>
+
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>    
+            </div>
+        </div>
+    </main>
+    
+</body>
+</html>
